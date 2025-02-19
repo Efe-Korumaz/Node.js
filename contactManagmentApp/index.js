c6ad28b3ab2dbea23d81f2705352d7473ef874ab
Efe korumaz <E.f.e.korumaz@gmail.com>
2025-02-19 10:24:33 +0100

    setup van project

---
+++ contactManagmentApp/index.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const dotenv = require("dotenv").config();
+app = express();
+
+const port = proccess.env.PORT || 3000;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(port, () =>
+    {
+        console.log(`listening to ${port}`)
+    })
